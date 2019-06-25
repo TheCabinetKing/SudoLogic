75094546776e1146285f8f4e0fe02bf5675a8eb0
Mistfader <haloglitchguy@gmail.com>
2019-06-26 11:24:10 +1200

    Test server now operational.

--- sumosv.py
+++ sumosv.py
@@ -1,9 +1,14 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
 
 @app.route('/alert',methods=['POST'])
 def getalert():
+    #Get message from posted json
     msg =  request.get_json(force=True)
-    for(keys,values in msg):
+    #Debug outputs to confirm reception
+    for keys, values in msg.items():
         print(keys)
         print(values)
+    #Return generic ack alongside 200 status.
     return jsonify(status="ACK")
